f31b07c908c9bb90b5b9a5af82b624ba1e345a15
Vijay Stroup <vijaystroup@gmail.com>
2023-02-17 20:26:19 -0500

    contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,24 +2,152 @@ import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
 import Head from 'next/head'
 import Layout from '../components/layout'
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/solid'
 
 const Contact: NextPageWithLayout = () => {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const { company, title, first_name, last_name, phone, email, comments } =
+      Object.fromEntries(formData)
+    console.log(company, title, first_name, last_name, phone, email, comments)
+  }
+
   return (
     <>
       <Head>
         <title>Contact - Gator</title>
       </Head>
 
-      <main className='flex flex-col items-center space-y-4'>
-        <div className='flex items-center space-x-2'>
-          <EnvelopeIcon className='w-6 h-6' />
-          <p className='text-2xl'>contact@gator.sh</p>
-        </div>
-        <div className='flex items-center space-x-2'>
-          <PhoneIcon className='w-6 h-6' />
-          <p className='text-2xl'>+1 (321) 430-8534</p>
-        </div>
+      <main>
+        <h1 className='text-2xl font-bold text-center'>Contact</h1>
+        <hr className='w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10' />
+
+        <form className='mt-10 text-black' onSubmit={handleSubmit}>
+          <div className='relative z-0 w-full mb-6 group'>
+            <input
+              type='text'
+              name='company'
+              id='company'
+              className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+              placeholder=' '
+              required
+            />
+            <label
+              htmlFor='company'
+              className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            >
+              Company (e.g. Google)
+            </label>
+          </div>
+          <div className='relative z-0 w-full mb-6 group'>
+            <input
+              type='text'
+              name='title'
+              id='title'
+              className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+              placeholder=' '
+              required
+            />
+            <label
+              htmlFor='title'
+              className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            >
+              Title (e.g. Supply Chain Manager)
+            </label>
+          </div>
+          <div className='grid md:grid-cols-2 md:gap-6'>
+            <div className='relative z-0 w-full mb-6 group'>
+              <input
+                type='text'
+                name='first_name'
+                id='first_name'
+                className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+                placeholder=' '
+                required
+              />
+              <label
+                htmlFor='first_name'
+                className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              >
+                First name
+              </label>
+            </div>
+            <div className='relative z-0 w-full mb-6 group'>
+              <input
+                type='text'
+                name='last_name'
+                id='last_name'
+                className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+                placeholder=' '
+                required
+              />
+              <label
+                htmlFor='last_name'
+                className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              >
+                Last name
+              </label>
+            </div>
+          </div>
+          <div className='grid md:grid-cols-2 md:gap-6'>
+            <div className='relative z-0 w-full mb-6 group'>
+              <input
+                type='tel'
+                pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+                name='phone'
+                id='phone'
+                className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+                placeholder=' '
+                required
+              />
+              <label
+                htmlFor='phone'
+                className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              >
+                Phone number (123-456-7890)
+              </label>
+            </div>
+            <div className='relative z-0 w-full mb-6 group'>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+                placeholder=' '
+                required
+              />
+              <label
+                htmlFor='email'
+                className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              >
+                Email address
+              </label>
+            </div>
+          </div>
+          <div className='relative z-0 w-full mb-6 group'>
+            <textarea
+              name='comments'
+              id='comments'
+              className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+              placeholder=' '
+              rows={3}
+            />
+            <label
+              htmlFor='comments'
+              className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            >
+              Extra Comments
+            </label>
+          </div>
+
+          <button
+            type='submit'
+            className='text-white float-right bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            Submit
+          </button>
+        </form>
       </main>
     </>
   )
