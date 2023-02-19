0a6e7a967c77bebec3b7f93e7782969b8e54ab33
Vijay Stroup <vijaystroup@gmail.com>
2023-02-18 21:54:47 -0500

    add new entry for contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,9 +8,26 @@ const Contact: NextPageWithLayout = () => {
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget)
-    const { company, title, first_name, last_name, phone, email, comments } =
-      Object.fromEntries(formData)
-    console.log(company, title, first_name, last_name, phone, email, comments)
+    const {
+      company,
+      title,
+      first_name,
+      last_name,
+      phone,
+      email,
+      solutions,
+      comments
+    } = Object.fromEntries(formData)
+    console.log(
+      company,
+      title,
+      first_name,
+      last_name,
+      phone,
+      email,
+      solutions,
+      comments
+    )
   }
 
   return (
@@ -124,6 +141,22 @@ const Contact: NextPageWithLayout = () => {
                 Email address
               </label>
             </div>
+          </div>
+          <div className='relative z-0 w-full mb-6 group'>
+            <input
+              type='text'
+              name='solutions'
+              id='solutions'
+              className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+              placeholder=' '
+              required
+            />
+            <label
+              htmlFor='solutions'
+              className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            >
+              Solution(s)
+            </label>
           </div>
           <div className='relative z-0 w-full mb-6 group'>
             <textarea
