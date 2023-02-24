6413595b601b45232efd2eaded25c190af204b08
Vijay Stroup <vijaystroup@gmail.com>
2023-02-24 18:22:53 -0500

    add eg

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -124,7 +124,7 @@ const Careers: NextPageWithLayout = () => {
                 htmlFor='portfolio'
                 className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               >
-                Portfolio (e.g. personal website, GitHub, etc.)
+                Portfolio (e.g. personal website, Resumé link, GitHub, etc.)
               </label>
             </div>
 
