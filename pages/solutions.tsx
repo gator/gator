0f630ccf2fd3e0036505a38e259d3224069f10af
Vijay Stroup <vijaystroup@gmail.com>
2023-02-26 12:38:15 -0500

    solutions page update

--- pages/solutions.tsx
+++ pages/solutions.tsx
@@ -1,5 +1,6 @@
 import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
+import Link from 'next/link'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
@@ -12,9 +13,9 @@ const Solutions: NextPageWithLayout = () => {
         <h1 className='text-2xl font-bold text-center'>Solutions</h1>
         <hr className='w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10' />
 
-        <div className='space-y-10'>
+        <div className='mt-10 space-y-10'>
           <div className='flex justify-between space-x-4'>
-            <img src='/Alligator2.png' className='w-14 h-14' />
+            <img src='/bednotes_logo.png' className='w-14 h-14' />
             <div className=''>
               <h2 className='text-lg font-bold'>Bed Notes</h2>
               <p>
@@ -38,23 +39,28 @@ const Solutions: NextPageWithLayout = () => {
           <div className='flex justify-between space-x-4'>
             <img src='/Alligator2.png' className='w-14 h-14' />
             <div className=''>
-              <h2 className='text-lg font-bold'>Bed Notes</h2>
+              <h2 className='text-lg font-bold'>Jonathan</h2>
               <p>
-                Bed Notes is a cloud-based solution that streamlines the process
-                of creating and managing patient care plans. It is designed to
-                help healthcare professionals create, edit, and share care plans
-                with their colleagues and patients. Bed Notes is a powerful tool
-                that enables healthcare professionals to improve patient care
-                and outcomes, while reducing the time and effort required to
-                create and manage care plans.
+                Jonathan is an AI assistant to help physicians and medical
+                professionals streamline the process of note-taking during
+                patient visits. By listening to the interaction between doctors
+                and patients, our software takes notes, summarizes the
+                conversation, and generates an accurate and comprehensive
+                report. With its advanced natural language processing and
+                machine learning capabilities, our tool can identify key
+                information, such as diagnoses, medications, and treatment
+                plans, and ensure that they are properly documented in the
+                patient's medical record. By eliminating the need for manual
+                note-taking and reducing the risk of errors or omissions, our AI
+                scribing tool can help doctors save time, improve accuracy, and
+                enhance the quality of care for their patients.
               </p>
-              <a
-                href='https://bednotes.gator.sh'
-                target='_blank'
-                className='text-blue-500 hover:underline'
-              >
-                Learn More
-              </a>
+              <p className='text-blue-500 hover:underline'>
+                Coming Soon...{' '}
+                <span>
+                  <Link href='/contact'>(Contact us for updates.)</Link>
+                </span>
+              </p>
             </div>
           </div>
         </div>
