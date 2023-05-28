79b6a1dde29661e64ddd27d456a2fca04bd528e6
Vijay Stroup <vijaystroup@gmail.com>
2023-05-28 15:51:30 -0400

    layout motion

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,28 @@
 import { type FC } from 'react'
 import Footer from './footer'
 import Header from './header'
+import { motion } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const router = useRouter()
+
   return (
-    <div className='flex flex-col h-screen'>
+    <motion.div
+      key={router.route}
+      className='flex flex-col h-screen'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       <Header />
       {children}
       <Footer />
-    </div>
+    </motion.div>
   )
 }
 
