c5337983428e3aad4b37c3310d737b4282b95614
Vijay Stroup <vijaystroup@gmail.com>
2023-05-28 18:01:29 -0400

    mission page update

--- pages/mission.tsx
+++ pages/mission.tsx
@@ -8,32 +8,22 @@ const Mission: NextPageWithLayout = () => {
     <>
       <SEO title='Mission - Gator' />
 
-      <main className='text-center'>
-        <h1 className='text-2xl font-black text-center'>Mission</h1>
-        <hr className='w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10' />
+      <img
+        src='/mission_background.jpg'
+        alt='mission_background'
+        className='absolute top-0 object-cover h-full'
+      />
 
-        <p>
-          Gator is dedicated to advancing healthcare technology through
-          innovative software solutions. Our cutting-edge products empower
-          healthcare professionals and organizations to streamline their
-          operations, enhance patient care, and achieve better outcomes. With a
-          deep understanding of the unique challenges and opportunities in the
-          healthcare industry, we develop software that is tailored to the needs
-          of our clients. Our team of experienced developers, designers, and
-          healthcare experts work collaboratively to create intuitive, reliable,
-          and scalable software that drives the evolution of the healthcare
-          ecosystem. Whether you're looking to improve clinical workflows, or
-          enhance patient engagement, Gator has the expertise and technology to
-          help you achieve your goals.
+      <main className='z-10 flex flex-col justify-center h-full text-center top-1/2'>
+        <h1 className='text-6xl font-black tracking-wider mb-14'>
+          The Mission.
+        </h1>
+        <p className='text-xl'>
+          The best care someone can receive is when the care giver is cared for
+          too. Helping the helpers goes a long way in the success of Health
+          Care.
         </p>
-
-        <h2 className='mt-10 mb-2 text-lg font-bold'>Our Goals</h2>
-        <hr className='w-24 h-1 mx-auto my-4 -translate-y-2 bg-gray-100 border-0 rounded md:my-8' />
-        <ol className='space-y-4 font-bold tracking-wider'>
-          <li>Simplicity.</li>
-          <li>User Friendliness.</li>
-          <li>Reliability.</li>
-        </ol>
+        <p className='mt-4 text-xl'>Help Everyone.</p>
       </main>
     </>
   )
