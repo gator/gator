3c1576b1fd75bd5cbd94fd0d32974f6305eec7a7
Vijay Stroup <vijaystroup@gmail.com>
2023-05-28 15:50:03 -0400

    wrap all pages with AnimatePresence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,10 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.scss'
+import { AnimatePresence } from 'framer-motion'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -17,7 +18,11 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <AnimatePresence mode='wait'>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  )
 }
 
 export default MyApp
