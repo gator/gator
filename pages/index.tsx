dc9ecf3cb84ffbc3a68762c511e08fd94465e4ea
Vijay Stroup <vijaystroup@gmail.com>
2023-05-28 00:30:57 -0400

    home page content update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPageWithLayout } from './_app'
 import Link from 'next/link'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -17,6 +18,17 @@ const Home: NextPageWithLayout = () => {
         className='object-cover h-full'
         onContextMenu={() => false}
       />
+
+      <main className='absolute flex flex-col items-center justify-center w-full h-full'>
+        <h1 className='text-5xl font-bold'>Health Care Helps Everyone</h1>
+        <Link
+          href='/join'
+          className='flex items-center gap-1 px-6 py-4 mt-5 text-2xl group'
+        >
+          Start Helping
+          <ChevronRightIcon className='w-6 h-6 duration-100 group-hover:translate-x-1' />
+        </Link>
+      </main>
     </>
   )
 }
