83953a65bd216585c977ba58f6afd02266ceea01
Vijay Stroup <vijaystroup@gmail.com>
2023-05-28 12:00:24 -0400

    extra menu component

---
+++ components/extraMenu.tsx
@@ -0,0 +1,45 @@
+import { type FC } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link'
+
+interface ExtraMenuProps {
+  showExtraMenu: boolean
+}
+
+const ExtraMenu: FC<ExtraMenuProps> = ({ showExtraMenu }) => {
+  return (
+    <div
+      className={`fixed flex bg-black border-[1px] mx-8 rounded-full px-4 py-2 items-center duration-200 top-0 right-0 z-20
+      ${showExtraMenu && 'translate-y-14'}
+    `}
+    >
+      <p className='mr-24 text-xs'>&copy; Gator, LLC All rights reserved.</p>
+
+      <nav>
+        <ul className='flex space-x-4'>
+          <li>
+            <Link
+              href='https://www.linkedin.com/company/gator'
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              <FontAwesomeIcon icon={faLinkedin} size='sm' color='white' />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href='https://github.com/gator'
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              <FontAwesomeIcon icon={faGithub} size='sm' color='white' />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default ExtraMenu
