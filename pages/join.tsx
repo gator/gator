32bf59fe3377d3a6d410102850742c2c7ebbf62c
Vijay Stroup <vijaystroup@gmail.com>
2023-05-28 20:23:39 -0400

    join page mobile styling

--- pages/join.tsx
+++ pages/join.tsx
@@ -39,7 +39,7 @@ const Join: NextPageWithLayout = () => {
 
       <main>
         <motion.h1
-          className='mt-40 text-6xl font-black'
+          className='mt-40 text-6xl font-black text-center md:text-left'
           initial={{ translateX: -15 }}
           animate={{ translateX: 0 }}
           transition={{ duration: 1 }}
