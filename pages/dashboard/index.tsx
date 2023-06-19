b028951945cfba5d1670cdd14895fc668ff198cf
Vijay Stroup <vijaystroup@gmail.com>
2023-06-18 20:32:23 -0400

    dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,6 +5,7 @@ import SEO from '../../components/seo'
 import { useUser } from '@clerk/nextjs'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { motion } from 'framer-motion'
 
 const Dashboard: NextPageWithLayout = () => {
   const { user, isLoaded } = useUser()
@@ -25,18 +26,35 @@ const Dashboard: NextPageWithLayout = () => {
     <>
       <SEO title='Dashboard - Gator' />
 
-      <main>
-        <h1 className='text-5xl'>Hi, {user.firstName}.</h1>
-        <h2 className='mt-10 mb-20 text-xl'>
+      <main className='h-full text-white bg-black'>
+        <motion.h1
+          className='text-5xl'
+          initial={{ translateX: -15 }}
+          animate={{ translateX: 0 }}
+          transition={{ duration: 0.75 }}
+        >
+          Hi, {user.firstName}.
+        </motion.h1>
+        <motion.h2
+          className='mt-10 mb-20 text-xl'
+          initial={{ translateX: -15 }}
+          animate={{ translateX: 0 }}
+          transition={{ duration: 1 }}
+        >
           Select the application you would like to go to.
-        </h2>
+        </motion.h2>
 
         <div className='flex justify-center gap-5'>
-          <Link href='/dashboard/scribe' className=''>
-            <div className='bg-white/5 border-[1px] border-white/50 rounded-lg p-20 w-fit hover:-translate-y-1 duration-100 text-center'>
+          <Link href='/dashboard/scribe'>
+            <motion.div
+              className='bg-white/5 border-[1px] border-white/50 rounded-lg p-20 w-fit hover:-translate-y-1 duration-100 text-center'
+              initial={{ translateY: 10, opacity: 0 }}
+              animate={{ translateY: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
               <p className='mb-1 text-sm'>Gator</p>
               <h1 className='text-3xl font-bold'>Scribe</h1>
-            </div>
+            </motion.div>
           </Link>
         </div>
       </main>
