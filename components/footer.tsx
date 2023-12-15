deca85017bdde88ad3033518726b4a1ffe90ad42
Vijay Stroup <vijaystroup@gmail.com>
2023-12-15 08:37:52 -0500

    Add social media icons to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,41 +1,75 @@
 import { type FC } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer: FC = () => {
-  const router = useRouter()
-
-  const darkPaths = [
-    '/privacy-policy',
-    '/terms-of-use',
-    '/login',
-    '/dashboard/scribe'
-  ]
-
   return (
-    <footer
-      className={`fixed bottom-0 right-0 z-10 mb-5 mr-5 text-sm group hidden sm:block
-        ${darkPaths.includes(router.pathname) ? 'text-black' : 'text-white'}
-      `}
-    >
-      <Link
-        href='/privacy-policy'
-        className={`p-2 duration-[150ms] group-hover:opacity-50 hover:!opacity-100 
-          ${
-            router.pathname === '/privacy-policy' ? 'opacity-100' : 'opacity-50'
-          }`}
-      >
-        Privacy
-      </Link>
-      <Link
-        href='/terms-of-use'
-        className={`p-2 duration-[150ms] group-hover:opacity-50 hover:!opacity-100 
-          ${
-            router.pathname === '/terms-of-use' ? 'opacity-100' : 'opacity-50'
-          }`}
-      >
-        Terms
-      </Link>
+    <footer className='bg-[#34c759] text-white pt-14 pb-24 px-4'>
+      <div className='flex mb-10 sm:flex-col sm:mb-0'>
+        <img
+          src='/images/logo_banner.svg'
+          className='w-auto h-10 mb-4 mr-4 sm:h-6 sm:rm-0'
+        />
+        <div className='space-x-6'>
+          <Link
+            href='https://www.linkedin.com/company/gator'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size='xl'
+              color='white'
+              className='translate-y-2'
+            />
+          </Link>
+          <Link
+            href='https://github.com/gator'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              size='xl'
+              color='white'
+              className='translate-y-2'
+            />
+          </Link>
+        </div>
+      </div>
+
+      <div className='space-y-10'>
+        <div>
+          <h1 className='mb-4 text-xl font-bold'>Menu</h1>
+          <ul className='space-y-2 opacity-80'>
+            <li>
+              <Link href='/#how-it-works'>How It Works</Link>
+            </li>
+            <li>
+              <Link href='/#features'>Features</Link>
+            </li>
+            <li>
+              <Link href='/#contact'>Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className='mb-4 text-xl font-bold'>Help</h1>
+          <ul className='space-y-2 opacity-80'>
+            <li>
+              <Link href='/privacy-policy'>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link href='/terms-of-use'>Terms of Use</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <p className='mt-20 text-sm text-center opacity-50'>
+        &copy; Gator, LLC All rights reserved.
+      </p>
     </footer>
   )
 }
