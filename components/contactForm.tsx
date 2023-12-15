7aade22f22d909182c18ca80e649afc674d44581
Vijay Stroup <vijaystroup@gmail.com>
2023-12-15 16:51:11 -0500

    re-order fields

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -56,38 +56,6 @@ const ContactForm: FC = () => {
           )}
 
           <form className='mt-10' onSubmit={handleSubmit}>
-            <div className='relative z-0 w-full mb-6 group'>
-              <input
-                type='text'
-                name='organization'
-                id='organization'
-                className='block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
-                placeholder=' '
-                required
-              />
-              <label
-                htmlFor='organization'
-                className='peer-focus:font-medium absolute text-sm text-black/50 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
-              >
-                Organization (e.g. Mayo Clinic)
-              </label>
-            </div>
-            <div className='relative z-0 w-full mb-6 group'>
-              <input
-                type='text'
-                name='title'
-                id='title'
-                className='block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
-                placeholder=' '
-                required
-              />
-              <label
-                htmlFor='title'
-                className='peer-focus:font-medium absolute text-sm text-black/50 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
-              >
-                Title (e.g. VP of Innovation)
-              </label>
-            </div>
             <div className='grid md:grid-cols-2 md:gap-6'>
               <div className='relative z-0 w-full mb-6 group'>
                 <input
@@ -103,6 +71,40 @@ const ContactForm: FC = () => {
                   className='peer-focus:font-medium absolute text-sm text-black/50 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
                 >
                   Name
+                </label>
+              </div>
+            </div>
+            <div className='grid md:grid-cols-2 md:gap-6'>
+              <div className='relative z-0 w-full mb-6 group'>
+                <input
+                  type='text'
+                  name='organization'
+                  id='organization'
+                  className='block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+                  placeholder=' '
+                  required
+                />
+                <label
+                  htmlFor='organization'
+                  className='peer-focus:font-medium absolute text-sm text-black/50 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                >
+                  Organization (e.g. Mayo Clinic)
+                </label>
+              </div>
+              <div className='relative z-0 w-full mb-6 group'>
+                <input
+                  type='text'
+                  name='title'
+                  id='title'
+                  className='block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer'
+                  placeholder=' '
+                  required
+                />
+                <label
+                  htmlFor='title'
+                  className='peer-focus:font-medium absolute text-sm text-black/50 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                >
+                  Title (e.g. VP of Innovation)
                 </label>
               </div>
             </div>
