57c821387b3074b1cb2983b8000a41bb8cd41f26
Vijay Stroup <vijaystroup@gmail.com>
2023-12-15 14:04:41 -0500

    Add countdown timer and update text styles in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Custom404() {
+  const [countDown, setCountDown] = useState(5)
+
   useEffect(() => {
     // redirect to home page after 5 seconds
     setTimeout(() => {
       window.location.href = '/'
     }, 5000)
+
+    const interval = setInterval(() => {
+      setCountDown((prev: number) => prev - 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
   }, [])
 
   return (
@@ -17,9 +27,10 @@ export default function Custom404() {
         loop
         className='absolute top-0 object-cover h-full -z-10'
       />
-      <main className=''>
-        <h1 className='mt-40 text-6xl font-bold'>Are you lost?</h1>
-        <p className='mt-10 text-2xl'>Here, let me help you.</p>
+      <main className='text-center text-white mt-60'>
+        <h1 className='font-bold text-9xl'>Are you lost?</h1>
+        <p className='mt-10 text-5xl'>Here, let me help you.</p>
+        <p className='mt-10 text-5xl'>Redirecting in {countDown} seconds.</p>
       </main>
     </>
   )
