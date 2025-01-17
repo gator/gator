05a8d890973086316cae19f88b7fd270ced04adc
Vijay Stroup <vijaystroup@gmail.com>
2025-01-17 13:52:05 -0500

    redirect to notion site for help

---
+++ app/help/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation'
+
+export default function Page() {
+  redirect(
+    'https://talented-advantage-3c8.notion.site/17ebdd4e046b80ddad31e4fb34e81019'
+  )
+}
